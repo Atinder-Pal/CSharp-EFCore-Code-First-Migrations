cd85766d23906282be1d5c1e959c4d05ad73badd
Atinder-Pal <atty.pal46@gmail.com>
2020-10-31 16:53:14 -0600

    Add InitialMigration; Apply Migration to create DB;

---
+++ CodeFirstPractice/CodeFirstPractice/Migrations/20201031225123_InitialMigration.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CodeFirstPractice.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Shelf",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int(10)", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "varchar(50)", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                        .Annotation("MySql:Collation", "utf8mb4_general_ci")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Shelf", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Shelf");
+        }
+    }
+}
